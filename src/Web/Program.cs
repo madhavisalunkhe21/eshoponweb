e4f3461fd10d7b8b55d18f0d0b69536cf13f16e7
madhavi <madhavi.salunkhe21@gmail.com>
2026-03-03 20:45:36 +0530

    test1

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -22,6 +22,7 @@ using Microsoft.eShopWeb.Web.Pages;
 using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
 
+// Testing
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole();
